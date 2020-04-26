cc1e34cd0dbeac6dbee6109e37723ece006529e0
Zeyu Ni <joey.ni827@gmail.com>
2020-04-26 08:29:21 -0700

    Create 0020. Search In Unknown Sized Sorted Array.java

---
+++ 0020. Search In Unknown Sized Sorted Array.java
@@ -0,0 +1,49 @@
+/*
+*  interface Dictionary {
+*    public Integer get(int index);
+*  }
+*/
+
+// You do not need to implement the Dictionary interface.
+// You can use it directly, the implementation is provided when testing your solution.
+public class Solution {
+  public int search(Dictionary dict, int target) {
+    if (dict == null) {
+      return -1;
+    }
+
+    int l = 0;
+    int r = 1;
+    // find the right boundary for binary search
+    // extends until we are sure the target is within the [l, r] range.
+    while (dict.get(r) != null && dict.get(r) < target) {
+      // 1. move l to r
+      // 2. double r index
+      l = r;
+      r = 2 * r;
+    }
+
+    return binarySearch(dict, target, l, r);
+  }
+
+  private int binarySearch(Dictionary dict, int target, int l, int r) {
+    // classical binary search
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+
+      if (dict.get(m) == null) {
+        r = m - 1;
+        continue;
+      }
+
+      if (dict.get(m) == target) {
+          return m;
+      } else if (dict.get(m) < target) {
+          l = m + 1;
+      } else {
+          r = m - 1;
+      }
+    }
+    return -1;
+  }
+}
