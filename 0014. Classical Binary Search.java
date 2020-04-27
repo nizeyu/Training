a8bd39cd77d33ac19452a63287fc46d0c3924db5
Zeyu Ni <joey.ni827@gmail.com>
2020-04-27 06:21:44 -0700

    Update 0014. Classical Binary Search.java

--- 0014. Classical Binary Search.java
+++ 0014. Classical Binary Search.java
@@ -4,18 +4,18 @@ public class Solution {
           return -1;
       }
 
-      int l = 0;
-      int r = array.length - 1;
-      while (l <= r) { // l < r is WRONG, eg. a[] = {5} target = 5
-        int m = l + (r - l) / 2;
+      int left = 0;
+      int right = array.length - 1;
+      while (left <= right) { // l < r is WRONG, eg. a[] = {5} target = 5
+        int mid = left + (right - left) / 2;
 
-        if (array[m] == target) {
-            return m;
-        } else if (array[m] < target) {
-            l = m + 1; // l = m; ??? eg. a[] = {5} target = 8
+        if (array[mid] == target) {
+            return mid;
+        } else if (array[mid] < target) {
+            left = mid + 1; // l = m; ??? eg. a[] = {5} target = 8
                        //                      LMR = 0
         } else {
-            r = m - 1;
+            right = mid - 1;
         }
       }
 
