45a7e578c5d9143d63c2262a0817fb375aafefa3
Zeyu Ni <joey.ni827@gmail.com>
2020-04-27 10:07:17 -0700

    Update 0020. Search In Unknown Sized Sorted Array.java

--- 0020. Search In Unknown Sized Sorted Array.java
+++ 0020. Search In Unknown Sized Sorted Array.java
@@ -11,6 +11,54 @@ public class Solution {
     if (dict == null) {
       return -1;
     }
+    
+    int left = 0;
+    int right = 1;
+    while (dict.get(right) != null && dict.get(right) < target) {
+      left = right;
+      right *= 2;
+    }
+
+    return binarySearch(dict, target, left, right);
+    
+  }
+
+  private int binarySearch(Dictionary dict, int target, int left, int right) {
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+
+      if (dict.get(mid) == null) {
+        right = mid - 1;
+        continue;
+      }
+
+      if (dict.get(mid) == target) {
+        return mid;
+      } else if (dict.get(mid) < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
+
+// My solution
+/*
+*  interface Dictionary {
+*    public Integer get(int index);
+*  }
+*/
+
+// You do not need to implement the Dictionary interface.
+// You can use it directly, the implementation is provided when testing your solution.
+public class Solution {
+  public int search(Dictionary dict, int target) {
+    if (dict == null) {
+      return -1;
+    }
 
     int l = 0;
     int r = 1;
