41e6fa43df5f2775143f04bb23773bf0b738c2e9
Zeyu Ni <joey.ni827@gmail.com>
2020-05-08 07:38:07 -0700

    Create 0036. Middle Node Of Linked List.java

---
+++ 0036. Middle Node Of Linked List.java
@@ -0,0 +1,26 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode middleNode(ListNode head) {
+    if (head == null) {
+      return head;
+    }
+
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+}
