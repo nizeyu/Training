c49dfab422bc25de90d1acbebe6dd2e32929cee5
Zeyu Ni <joey.ni827@gmail.com>
2020-05-08 14:08:51 -0700

    Create 0223. Add Two Numbers.java

---
+++ 0223. Add Two Numbers.java
@@ -0,0 +1,35 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    
+    ListNode dummyNode = new ListNode(0);
+    ListNode prev = dummyNode;
+    int val = 0;
+    while (l1 != null || l2 != null || val != 0) {
+      if (l1 != null) {
+        val += l1.value;
+        l1 = l1.next;
+      }
+
+      if (l2 != null) {
+         val += l2.value;
+        l2 = l2.next;
+      }
+
+      prev.next = new ListNode(val % 10);
+      val = val / 10;
+      prev = prev.next;
+    }
+
+    return dummyNode.next;
+  }
+}
