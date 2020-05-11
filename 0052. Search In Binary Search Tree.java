45bbcda393312344cc149ed5baad139c4c1173af
Zeyu Ni <joey.ni827@gmail.com>
2020-05-11 08:02:26 -0700

    Create 0052. Search In Binary Search Tree.java

---
+++ 0052. Search In Binary Search Tree.java
@@ -0,0 +1,53 @@
+// Iteration
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode search(TreeNode root, int key) {
+    while (root != null) {
+      if (root.key == key) {
+        return root;
+      }
+
+      if (root.key < key) {
+        root = root.right;
+      } else {
+        root = root.left;
+      }
+    }
+
+    return root;
+  }
+}
+
+// Recursion 
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode search(TreeNode root, int key) {
+    if (root == null || root.key == key) {
+      return root;
+    }
+
+    if (root.key < key) {
+      return search(root.right, key);
+    } else {
+      return search(root.left, key);
+    }
+  }
+}
