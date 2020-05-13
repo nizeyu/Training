bbc52eb7ebd308b0861f69dfe2235434c784d113
Zeyu Ni <joey.ni827@gmail.com>
2020-05-13 16:17:18 -0700

    Create 0053. Delete In Binary Search Tree.java

---
+++ 0053. Delete In Binary Search Tree.java
@@ -0,0 +1,52 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode deleteTree(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
+
+    if (root.key > key) {
+      root.left = deleteTree(root.left, key);
+      return root;
+    } else if (root.key < key) {
+      root.right = deleteTree(root.right, key);
+      return root;
+    }
+
+    if (root.left == null) {
+      return root.right;
+    } else if (root.right == null) {
+      return root.left;
+    }
+
+    if (root.right.left == null) {
+      root.right.left = root.left;
+      return root.right;
+    }
+
+    TreeNode smallest = deleteSmallest(root.right);
+    smallest.left = root.left;
+    smallest.right = root.right;
+    return smallest;
+  }
+
+  private TreeNode deleteSmallest(TreeNode cur) {
+    TreeNode prev = cur;
+    cur = cur.left;
+    while (cur.left != null) {
+      prev = cur;
+      cur = cur.left;
+    }
+    prev.left = prev.left.right;
+    return cur;
+  }
+}
