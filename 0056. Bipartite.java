0f9118d6fc6608595207dfdbc3b2d5a42165d617
Zeyu Ni <joey.ni827@gmail.com>
2020-05-16 15:22:14 -0700

    Update 0056. Bipartite.java

--- 0056. Bipartite.java
+++ 0056. Bipartite.java
@@ -10,6 +10,58 @@
  */
 public class Solution {
   public boolean isBipartite(List<GraphNode> graph) {
+    
+    Map<GraphNode, Integer> visited = new HashMap<>();
+
+    for (GraphNode node : graph) {
+      if (!BFS(node, visited)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean BFS(GraphNode node, Map<GraphNode, Integer> visited) {
+    if (visited.containsKey(node)) {
+      return true;
+    }
+
+    Queue<GraphNode> queue = new LinkedList<>();
+    queue.offer(node);
+    visited.put(node, 0);
+
+    while (!queue.isEmpty()) {
+      GraphNode cur = queue.poll();
+
+      int curGroup = visited.get(cur);
+      int neiGroup = curGroup == 0 ? 1 : 0;
+
+      for (GraphNode nei : cur.neighbors) {
+        if (!visited.containsKey(nei)) {
+          visited.put(nei, neiGroup);
+          queue.offer(nei);
+        } else if (visited.get(nei) != neiGroup) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
+
+/**
+ * public class GraphNode {
+ *   public int key;
+ *   public List<GraphNode> neighbors;
+ *   public GraphNode(int key) {
+ *     this.key = key;
+ *     this.neighbors = new ArrayList<GraphNode>();
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean isBipartite(List<GraphNode> graph) {
     if (graph == null || graph.length == 0) {
       return true;
     }
