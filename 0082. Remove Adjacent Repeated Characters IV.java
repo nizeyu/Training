0e7fc85fbb20077b1ad878588ff08fc4a47f33ac
Zeyu Ni <joey.ni827@gmail.com>
2020-05-23 16:33:29 -0700

    Update 0082. Remove Adjacent Repeated Characters IV.java

--- 0082. Remove Adjacent Repeated Characters IV.java
+++ 0082. Remove Adjacent Repeated Characters IV.java
@@ -5,6 +5,28 @@ public class Solution {
     }
 
     char[] array = input.toCharArray();
+    int slow = 0;
+    for (int fast = 1; fast < array.length; fast++) {
+      if (slow == -1 || array[fast] != array[slow]) {
+        array[++slow] = array[fast];
+      } else {
+        slow--;
+        while (fast + 1 < array.length && array[fast] == array[fast + 1]) {
+          fast++;
+        }
+      }
+    }
+    return new String(array, 0, slow + 1);
+  }
+}
+
+public class Solution {
+  public String deDup(String input) {
+    if (input == null || input.length() <= 1) {
+      return input;
+    }
+
+    char[] array = input.toCharArray();
     Deque<Character> stack = new ArrayDeque<>();
     int fast = 0;
     while (fast < array.length) {
