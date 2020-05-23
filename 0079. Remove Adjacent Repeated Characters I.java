13fafe20d62d1f8d7f5e71c736e34d94e4cbb9b0
Zeyu Ni <joey.ni827@gmail.com>
2020-05-23 05:12:06 -0700

    Create 0079. Remove Adjacent Repeated Characters I.java

---
+++ 0079. Remove Adjacent Repeated Characters I.java
@@ -0,0 +1,18 @@
+public class Solution {
+  public String deDup(String input) {
+    if (input == null) {
+      return input;
+    }
+    
+    char[] array = input.toCharArray();
+
+    int slow = 0;
+    for (int fast = 0; fast < array.length; fast++) {
+      if (fast == 0 || (fast != 0 && array[fast] != array[fast - 1])) {
+        array[slow++] = array[fast];
+      }
+    }
+
+    return new String(array, 0, slow);
+  }
+}
