2893bfbd89cedb2fc246274f06990901c092d239
Zeyu Ni <joey.ni827@gmail.com>
2020-06-02 21:08:14 -0700

    Create 0 126. Lowest Common Ancestor I.java

---
+++ 0 126. Lowest Common Ancestor I.java
@@ -0,0 +1,27 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root,
+      TreeNode one, TreeNode two) {
+    if (root == null || root == one || root == two) {
+      return root;
+    }
+
+    TreeNode left = lowestCommonAncestor(root.left, one, two);
+    TreeNode right = lowestCommonAncestor(root.right, one, two);
+
+    if (left != null && right != null) {
+      return root;
+    }
+
+    return left != null ? left : right;
+  }
+}
