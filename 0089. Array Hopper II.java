2696a212eb9dabffc8fa67fdc2e244c0e469c7b8
Zeyu Ni <joey.ni827@gmail.com>
2020-06-13 10:34:27 -0700

    Update 0089. Array Hopper II.java

--- 0089. Array Hopper II.java
+++ 0089. Array Hopper II.java
@@ -6,6 +6,36 @@ public class Solution {
     }
 
     int[] dp = new int[n];
+    dp[n - 1] = 0;
+    for (int i = n - 2; i >= 0; i--) {
+      dp[i] = -1;
+      if (i + array[i] > n - 1) {
+        dp[i] = 1;
+      } else {
+        int min = Integer.MAX_VALUE;
+        for (int j = array[i]; j >= 1; j--) {
+          if (dp[i + j] != -1) {
+            min = Math.min(min, dp[i + j]);
+          } 
+        }
+        if (min != Integer.MAX_VALUE) {
+          dp[i] = min + 1;
+        }
+      } 
+    }
+
+    return dp[0];
+  }
+}
+
+public class Solution {
+  public int minJump(int[] array) {
+    int n = array.length;
+    if (n == 1) {
+      return 0;
+    }
+
+    int[] dp = new int[n];
     dp[0] = 0;
     for (int i = 1; i < n; i++) {
       dp[i] = -1;
