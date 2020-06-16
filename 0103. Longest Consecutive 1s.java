a2a61ad5c012ece21c3e62c098c702fbac42ce43
Zeyu Ni <joey.ni827@gmail.com>
2020-06-16 16:05:49 -0700

    Update 0103. Longest Consecutive 1s.java

--- 0103. Longest Consecutive 1s.java
+++ 0103. Longest Consecutive 1s.java
@@ -1,5 +1,24 @@
 public class Solution {
   public int longest(int[] array) {
+    int max = 0;
+    int cur = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == 1) {
+        if (i == 0 || array[i - 1] == 0) {
+          cur = 1;
+        } else {
+          cur++;
+        }
+        max = Math.max(max, cur);
+      }
+    }
+
+    return max;
+  }
+}
+
+public class Solution {
+  public int longest(int[] array) {
     if (array.length == 0) {
       return 0;
     }
